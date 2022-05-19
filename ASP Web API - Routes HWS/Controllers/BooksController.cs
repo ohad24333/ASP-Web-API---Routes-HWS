2a5ae9cbd535da0d313393aeeb9e011597e8cd62
אוהד סעדיה <ohad24333@gmail.com>
2022-05-19 03:03:34 +0300

    Add project files.

---
+++ ASP Web API - Routes HWS/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Web_API___Routes_HWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(5)}")]
+        public  string GetMoreThenFive(int id)
+        {
+            return "Id More Then 5";
+        }
+
+        [Route("{id:regex((zoom|boom))}")]
+        public string GetZoomOrBoom(string id)
+        {
+            return id;
+        }
+    }
+}
